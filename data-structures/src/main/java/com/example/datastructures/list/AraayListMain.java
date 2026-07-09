3897cbf1b9111417965ad66eec0bff060715fc22
ahmed omar <ahmedomar1997.aoo@gmail.com>
2026-07-09 12:19:35 +0300

    first data structures

---
+++ data-structures/src/main/java/com/example/datastructures/list/AraayListMain.java
@@ -0,0 +1,25 @@
+package com.example.datastructures.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AraayListMain {
+
+public static void main(String[] args) {
+
+
+    List<String> names = new ArrayList<>();
+
+    names.add("Ahmed");
+    names.add("Bob");
+    names.add("Mohamed");
+    names.add("Jimmy");
+    names.set(1,"Ronaldo");
+    names.add(2,"Mahmoud");
+
+    System.out.println(names);
+
+
+
+    }
+}
