b4aa0d8bb4f2136303d66508ec274d7cd7f11c37
ahmedomar201 <ahmedomar1997.aoo@gmail.com>
2026-07-10 20:01:32 +0300

    update data structures

--- data-structures/src/main/java/com/example/datastructures/list/AraayListMain.java
+++ data-structures/src/main/java/com/example/datastructures/list/AraayListMain.java
@@ -9,6 +9,7 @@ public static void main(String[] args) {
 
 
     List<String> names = new ArrayList<>();
+    List<String> surnames = new ArrayList<>();
 
     names.add("Ahmed");
     names.add("Bob");
@@ -16,6 +17,10 @@ public static void main(String[] args) {
     names.add("Jimmy");
     names.set(1,"Ronaldo");
     names.add(2,"Mahmoud");
+
+    surnames.add("Olise");
+    surnames.add("ali");
+    names.addAll(surnames);
 
     System.out.println(names);
 
