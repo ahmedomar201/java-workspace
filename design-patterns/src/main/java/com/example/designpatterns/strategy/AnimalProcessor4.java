752e6a2c8a14e78ce3b0225133710bbbc1728e5e
ahmedomar201 <ahmedomar1997.aoo@gmail.com>
2026-09-14 23:11:01 +0300

    strategy pattern

--- design-patterns/src/main/java/com/example/designpatterns/strategy/AnimalProcessor4.java
+++ design-patterns/src/main/java/com/example/designpatterns/strategy/AnimalProcessor4.java
@@ -5,20 +5,28 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Slf4j
 @Component
-@RequiredArgsConstructor
 //delegator class
 public class AnimalProcessor4 {
 
     //constructor injection
-    private final ApplicationContext applicationContext;
+    private final List<AnimalService> animalServices;
 
+
+    public AnimalProcessor4(ApplicationContext applicationContext) {
+        animalServices = applicationContext
+                .getBeansOfType(AnimalService.class)
+                .values()
+                .stream()
+                .toList();
+    }
 
     //AnimalProcessor1 Delegate any animal class (DogStrategy,LionStrategy) to feed or makeSound
     public void feedAnimal(final Animals animals) throws Animals.AnimalException {
-        applicationContext.getBeansOfType(AnimalService.class)
-                .values()
+        animalServices
                 .stream()
                 .filter(animalService -> animalService.isType(animals))
                 .findFirst()
@@ -33,8 +41,7 @@ public class AnimalProcessor4 {
 
     public void makeSound(final Animals animals) throws Animals.AnimalException {
 
-        applicationContext.getBeansOfType(AnimalService.class)
-                .values()
+        animalServices
                 .stream()
                 .filter(animalService -> animalService.isType(animals))
                 .findFirst()
